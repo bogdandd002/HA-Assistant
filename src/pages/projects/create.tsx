cd60ee8c0398cace5bb834332c6a155196f4ecf2
bogdandd002 <156956865+bogdandd002@users.noreply.github.com>
2025-05-22 17:28:50 +0100

    create project issue sorted

--- src/pages/projects/create.tsx
+++ src/pages/projects/create.tsx
@@ -1,21 +1,43 @@
 import { Create, useForm, useSelect } from "@refinedev/antd";
 import MDEditor from "@uiw/react-md-editor";
-import { Form, Input, Select } from "antd";
-import { IContractor } from "../../interfaces/data/data";
+import { DatePicker, Form, Input, Select } from "antd";
+import { IContractor, IProject } from "../../interfaces/data/data";
+import dayjs from "dayjs";
+import useGetUserIdentity from "../../store/user_data";
 
 export const ProjectCreate = () => {
-  const { formProps, saveButtonProps } = useForm<IContractor>({});
+  const user = useGetUserIdentity.getState().user;
+  const { formProps, saveButtonProps, form, onFinish } = useForm<IProject>({});
 
-  const { selectProps: categorySelectProps } = useSelect({
-    resource: "categories",
-  });
+   const handleOnFinish = () => {
+    
+    form.setFieldsValue({
+      "contractors": user?.contractor_documentId
+      // contractor: contractor, //set contractor id same as the user crating
+    });
+
+    return form.getFieldsValue(true);
+  };
 
   return (
     <Create
-    title="Add Contractor"
+    title="Add Project"
     saveButtonProps={saveButtonProps}>
-      <Form {...formProps} layout="vertical">
+      <Form {...formProps} layout="vertical"
+      form={form}
+      onFinish={() => onFinish?.(handleOnFinish())}>
         <Form.Item
+          label={"Proj. Number"}
+          name={["project_nr"]}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input/>
+          </Form.Item>
+              <Form.Item
           label={"Name"}
           name={["name"]}
           rules={[
@@ -38,16 +60,35 @@ export const ProjectCreate = () => {
           <Input />
         </Form.Item>
         <Form.Item
-          label={"Activity"}
-          name={["activity"]}
+          label="Start date"
+          name={["start_date"]}
           rules={[
             {
-              required: true,
+              required: false,
             },
           ]}
+          getValueProps={(value) => ({
+            value: value ? dayjs(value) : undefined,
+          })}
         >
-          <Input />
+          <DatePicker format={"DD-MM-YYYY"} />
         </Form.Item>
+        <Form.Item
+          label="End date"
+          name={["end_date"]}
+          rules={[
+            {
+              required: false,
+            },
+          ]}
+          getValueProps={(value) => ({
+            value: value ? dayjs(value) : undefined,
+          })}
+        >
+          <DatePicker format={"DD-MM-YYYY"} />
+        </Form.Item>
+        <Form.Item
+        name={["contractors"]}></Form.Item>
       </Form>
     </Create>
   );
